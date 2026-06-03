0d925c14fefaacb1e4d6721f8c5140cd788efb0c
Leo Crixi <leandro.crixi@hotmail.com>
2026-06-03 23:42:20 +0200

    Header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import NFLLogo from "../../assets/National_Football_League_logo.svg";
+import style from "./Header.module.css";
+import components from '../../styles/components/components.module.css'
 
 const Header = () => {
   return (
-    <div>
-      <h1>This is the header</h1>
-    </div>
-  )
-}
+    <header>
+      <nav className={style.nav}>
+        <img src={NFLLogo} alt="" />
+        <ul>
+          <li>Docs</li>
+          <li>About</li>
+          <li className={`${components.btn} ${components.btnPrimary} ${style.support}`}>Support</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
